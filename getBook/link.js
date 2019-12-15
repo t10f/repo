8426aa4046c4185a7ffd0625100d04b3d721aa02
t10f <58892533+t10f@users.noreply.github.com>
2019-12-16 00:10:21 +0800

    Create link.js

---
+++ getBook/link.js
@@ -0,0 +1,19 @@
+$("#goto1").click(function() {
+  location.href = 'https://www.lazada.com.ph/products/master-the-wards-internal-medicine-handbook-e-book-i480442325-s1255212251.html';
+});
+
+$("#goto2").click(function() {
+  location.href = 'https://www.lazada.com.ph/products/the-unofficial-guide-to-radiology-read-xrays-confidently-i481968131-s1259984295.html';
+});
+
+$("#goto3").click(function() {
+  location.href = 'https://www.lazada.com.ph/products/ecg-mastery-bundle-free-ecg-rhythm-cheat-sheet-i480414253-s1255030623.html';
+});
+
+$("#goto4").click(function() {
+  location.href = 'https://www.lazada.com.ph/products/medical-symptoms-a-visual-guide-the-easy-way-to-identify-medical-problems-ebook-i455830386-s1174594922.html';
+});
+
+$("#goto5").click(function() {
+  location.href = 'https://www.lazada.com.ph/products/medical-student-survival-skills-ecg-ebook-i444934598-s1123094169.html';
+});
